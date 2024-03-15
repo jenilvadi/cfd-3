70c30ba98643534268ec25e057458e25bf39ed89
jenilvadi <vadijenil65@gmail.com>
2024-03-15 15:30:24 +0530

    day9

--- day9/pr4.c
+++ day9/pr4.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 void main()
 {
-    int i=1,y;
+    int i;
 
     printf("enter the number");
-    scanf("%d",&y);
+    scanf("%d",&i);
 
-    while (i<=y)
+    while (i>=0)
     {
         if(i%2!=0)
         {
         printf("%d\n",i);
         }
-        i++;
+        i--;
     }
     
 }
